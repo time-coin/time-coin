140b09fc4b6150ff95791c66685c502601f1e78e
William Corless <wmcorless@gmail.com>
2025-10-20 13:11:28 -0700

    fix(crypto): use rand_core::OsRng and pin rand_core=0.9 to resolve CI macOS RNG trait mismatch

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,7 +1,7 @@
 //! TIME Coin Cryptography
 
 use ed25519_dalek::{Signature, Signer, SigningKey, Verifier, VerifyingKey};
-use rand::rngs::OsRng;
+use rand_core::OsRng;
 use sha2::{Digest, Sha256};
 use thiserror::Error;
 
