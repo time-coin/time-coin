b124df2effdd844c84b3a04e19099aae70b67b8a
William Corless <wmcorless@gmail.com>
2025-10-23 19:07:53 -0700

    debug: Add logging for peer add/remove

--- network/src/manager.rs
+++ network/src/manager.rs
@@ -34,10 +34,12 @@ impl PeerManager {
                 
                 self.peers.write().await.insert(peer_addr, info.clone());
             println!("DEBUG: Added peer {} with version {}", peer_addr, info.version);
+            println!("DEBUG: Added peer {} with version {}", peer_addr, info.version);
                 
                 let peers_clone = self.peers.clone();
                 tokio::spawn(async move {
                     conn.keep_alive().await;
+            println!("DEBUG: Removing peer {}", peer_addr);
             println!("DEBUG: Removing peer {}", peer_addr);
                     peers_clone.write().await.remove(&peer_addr);
                 });
@@ -66,6 +68,7 @@ impl PeerManager {
 
     pub async fn add_connected_peer(&self, peer: PeerInfo) {
         self.peers.write().await.insert(peer.address, peer);
+            println!("DEBUG: Added peer {} with version {}", peer_addr, info.version);
             println!("DEBUG: Added peer {} with version {}", peer_addr, info.version);
     }
 
